a8a08c0762c3dc670d1ff9e767fc1be0c90dd43f
Jaid <jaid.jsx@gmail.com>
2020-04-14 09:04:52 +0200

    Testing changes

---
+++ src/lib/KeywordsProperty.js
@@ -0,0 +1,13 @@
+import Property from "lib/Property"
+
+export default class HomepageProperty extends Property {
+
+  getPkgKey() {
+    return "homepage"
+  }
+
+  getRepositoryKey() {
+    return "topics"
+  }
+
+}
