783c64bbc6ba63baef4d6e636e4c15aee32110e3
Jaid <jaid.jsx@gmail.com>
2022-05-27 23:30:57 +0200

    Migrated to new chalk
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/chalk.js
+++ src/lib/chalk.js
@@ -1,8 +1,8 @@
-import chalk from "chalk"
+import {Chalk} from "chalk"
 
 // GitHub Actions CI supports color, chalk just does not know that
 // So we force a certain chalk level
-const customChalk = new chalk.Instance({
+const customChalk = new Chalk({
   level: 2,
 })
 
